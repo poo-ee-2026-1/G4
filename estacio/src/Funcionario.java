d794f7bfda11069e211acf9d109d22ae05da3432
cesarneves45 <cesar_neves@discente.ufg.br>
2026-05-18 09:58:24 -0300

    Add files via upload

---
+++ estacio/src/Funcionario.java
@@ -0,0 +1,32 @@
+public class Funcionario extends Pessoa {
+
+    private String login;
+    private String senha;
+
+    public Funcionario(
+            String nome,
+            String login,
+            String senha) {
+
+        super(nome);
+
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public boolean autenticar(
+            String login,
+            String senha) {
+
+        return this.login.equals(login)
+            && this.senha.equals(senha);
+    }
+
+    @Override
+    public void exibirTipo() {
+
+        System.out.println(
+            "Usuário do tipo FUNCIONÁRIO"
+        );
+    }
+}
